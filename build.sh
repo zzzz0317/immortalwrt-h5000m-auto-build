a546d19f77594c7e43ea84b7ed0c648d6b4fb435
Zhe Zhang <zhangzhe0177@gmail.com>
2025-12-22 02:36:52 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd immortalwrt
+echo "update feeds"
+./scripts/feeds update -a || { echo "update feeds failed"; exit 1; }
+echo "install feeds"
+./scripts/feeds install -a || { echo "install feeds failed"; exit 1; }
+./scripts/feeds install -a -f -p qmodem || { echo "install qmodem feeds failed"; exit 1; }
+cat ../h5000m.config > .config
+echo "make defconfig"
+make defconfig || { echo "defconfig failed"; exit 1; }
+echo "diff initial config and new config:"
+diff ../h5000m.config .config
+echo "diff initial config and new config (from old config only):"
+diff ../h5000m.config .config | grep -e "^<" | grep -v "^< #"
+echo "diff initial config and new config (from new config only):"
+diff ../h5000m.config .config | grep -e "^>" | grep -v "^> #"
+echo "check device exist"
+grep -Fxq "CONFIG_TARGET_mediatek_filogic_DEVICE_hiveton_h5000m=y" .config || exit 1
+
+year=$(date +%y)
+month=$(date +%-m)
+day=$(date +%-d)
+hour=$(date +%-H)
+zz_build_date=$(date "+%Y-%m-%d %H:%M:%S %z")
+zz_build_uuid=$(uuidgen)
+
+echo "zz_build_date=${zz_build_date}"
+echo "zz_build_uuid=${zz_build_uuid}"
+echo "ZZ_BUILD_ID='${zz_build_uuid}'" > files/etc/zz_build_id
+echo "ZZ_BUILD_HOST='$(hostname)'" >> files/etc/zz_build_id
+echo "ZZ_BUILD_USER='$(whoami)'" >> files/etc/zz_build_id
+echo "ZZ_BUILD_DATE='${zz_build_date}'" >> files/etc/zz_build_id
+echo "ZZ_BUILD_REPO_HASH='$(cd .. && git rev-parse HEAD)'" >> files/etc/zz_build_id
+echo "ZZ_BUILD_IMM_HASH='$(git rev-parse HEAD)'" >> files/etc/zz_build_id
+echo "make immortalwrt"
+make V=0 -j$(nproc) || { echo "make failed"; exit 1; }
